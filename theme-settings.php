f6b3023a2c713c35373246ff7cdb47a6269971c0
Mikkel Ricky <rimi@aarhus.dk>
2015-09-18 16:36:28 +0200

    Issue #LOOP-185 by rimi-itk: Added "green" skin

--- theme-settings.php
+++ theme-settings.php
@@ -7,6 +7,7 @@ function loop_skins_form_system_theme_settings_alter(&$form, $form_state) {
       '' => t('(default)'),
       'red' => t('Red'),
       'blue' => t('Blue'),
+      'green' => t('Green'),
     ),
     '#default_value' => theme_get_setting('loop_skins_skin'),
     '#description'   => t('Choose a skin for the site.'),
