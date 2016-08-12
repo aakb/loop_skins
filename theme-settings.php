021421c7831545f0355fae50d78e19f48079bc3b
Mikkel Ricky <rimi@aarhus.dk>
2016-08-12 14:02:33 +0200

    Added theme for "Børn og Unge"

--- theme-settings.php
+++ theme-settings.php
@@ -9,6 +9,7 @@ function loop_skins_form_system_theme_settings_alter(&$form, $form_state) {
       'blue' => t('Blue'),
       'green' => t('Green'),
       'cura' => t('Cura'),
+      'buloop' => t('Børn og Unge'),
     ),
     '#default_value' => theme_get_setting('loop_skins_skin'),
     '#description'   => t('Choose a skin for the site.'),
